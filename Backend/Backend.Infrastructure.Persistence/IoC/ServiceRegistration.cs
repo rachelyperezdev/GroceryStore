c0b33f4d4f4a931e29cd312e351fe6e2e8b49489
RachelyPerez <68482998+rachelyperez31@users.noreply.github.com>
2024-09-26 22:50:19 -0400

    Add ServiceRegistration to Persistence Layer

--- Backend/Backend.Infrastructure.Persistence/IoC/ServiceRegistration.cs
+++ Backend/Backend.Infrastructure.Persistence/IoC/ServiceRegistration.cs
@@ -1,13 +1,34 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Backend.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Backend.Infrastructure.Persistence.IoC
 {
     public static class ServiceRegistration
     {
-        public static void AddInfrastructurePersistenceLayer(this IServiceCollection services, IConfiguration configuration)
+        public static void AddPersistenceInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
         {
+            #region Contexts
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<ApplicationContext>(options =>
+                {
+                    options.UseInMemoryDatabase("ApplicationDb");
+                });
+            }
+            else
+            {
+                services.AddDbContext<ApplicationContext>(options =>
+                {
+                    options.UseSqlServer(configuration.GetConnectionString("Default"),
+                        m => m.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName));
+                });
+            }
+            #endregion
 
+            #region Repositories
+            #endregion
         }
     }
 }
