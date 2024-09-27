0e69103af8b19f871a75996de526ef0a2d48db2a
RachelyPerez <68482998+rachelyperez31@users.noreply.github.com>
2024-09-27 11:09:21 -0400

    Add Ingredients DTOs.

---
+++ Backend/Backend.Core.Application/DTOs/Ingredient/CreateIngredientDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Core.Application.DTOs.Ingredient
+{
+    public class CreateIngredientDTO
+    {
+        [Required]
+        [MaxLength(300, ErrorMessage = "Name max length is 300 characters")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(1000, MinimumLength = 2, ErrorMessage = "Description max length is 1000 characters")]
+        public string Description { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+        public decimal Price { get; set; }
+        [Required]
+        [Range(0, double.MaxValue)]
+        public int Stock { get; set; }
+    }
+}
---
+++ Backend/Backend.Core.Application/DTOs/Ingredient/IngredientDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Core.Application.DTOs.Ingredient
+{
+    public class IngredientDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
