c6c658ba4b458f16a3f4fb20bf6c51562395cc06
RachelyPerez <68482998+rachelyperez31@users.noreply.github.com>
2024-09-27 18:55:40 -0400

    Add Swagger Extensions.

---
+++ Backend/Backend.WebApi/Extensions/AppExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Backend.WebApi.Extensions
+{
+    public static class AppExtensions
+    {
+        public static void UseSwaggerExtensions(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("swagger/v1/swagger.json", "Grocery Store App API");
+            });
+        }
+    }
+}
